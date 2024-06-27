8c31ae515f5ebd959c39e61d733db819b7800646
vicbradley <vicbradley33@gmail.com>
2024-06-27 21:20:04 +0800

    refactor code

--- src/user/user.services.js
+++ src/user/user.services.js
@@ -2,14 +2,13 @@ import { findUserById, updateUserById } from "./user.repository.js";
 
 export const getUserById = async (userId) => {
   const user = await findUserById(userId);
-  if (!user) throw Error("User not found");
+  if (!user) throw Error("User not found!");
 
   return user;
 };
 
 export const editUserById = async (userId, userNewData) => {
-  const user = await getUserById(userId);
-  if (!user) throw Error("User not found");
+  await getUserById(userId);
 
   const updatedUser = await updateUserById(userId, userNewData);
   return updatedUser;
