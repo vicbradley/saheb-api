d0f54a5754fad34024934804a91c5f10a566a70a
vicbradley <vicbradley33@gmail.com>
2024-07-16 13:29:13 +0800

    feat: add transaction route

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import config from "./config.js";
 import productController from "./src/product/product.controller.js";
 import consultController from "./src/consult/consult.controller.js";
 import { storeController } from "./src/store/store.controller.js";
 import { userController } from "./src/user/user.controller.js";
 import { chatroomController } from "./src/chatroom/chatroom.controller.js";
 import { cartController } from "./src/cart/cart.controller.js";
+import { transactionController } from "./src/transaction/transaction.controller.js";
 
 const app = express();
 
@@ -23,6 +23,7 @@ app.use("/stores", storeController);
 app.use("/users", userController);
 app.use("/chatrooms", chatroomController);
 app.use("/cart", cartController);
+app.use("/transactions", transactionController);
 
 app.listen(5000, () => console.log(`Server is live @localhost: 5000`));
 
